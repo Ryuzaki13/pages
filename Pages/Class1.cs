71064ff9653b98cc348b9133f6ad2240064efc63
Alec Afanasev <ryuzaki_dk_rm_guitar@mail.ru>
2021-09-07 09:09:46 +0500

    Add files via upload

---
+++ Pages/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4.Pages
+{
+    class Class1
+    {
+        public static Login PageLogin = new Login();
+        public static Registration PageReg = new Registration();
+    }
+}
